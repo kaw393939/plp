011c059a4e651058687d21a34745cc8a57baf9b0
T. Budiman <tbudiman@gmail.com>
2013-04-16 23:23:56 +0700

    change singleton construct

--- ipident/index.js
+++ ipident/index.js
@@ -1,7 +1,116 @@
 "use strict";
 
-var ipidentlib = require('./lib/ipident'),
-    ipident = ipidentlib.ipidentSingleton.getInstance();
+var fs = require('fs');
+var csv = require('csv');
+var redis = require('redis');
+var inet = require('./lib/inet');
 
-module.exports = ipident;
+// ipident is a Singleton
+var ipidentSingleton = (function () {
+
+    // reference to the Singleton
+    var instance;
+
+    function init() {
+
+        var client = redis.createClient();
+
+        client.on('error', function (error) {
+            console.log(error.message);
+        });
+
+        return {
+
+            clearData: function (callback) {
+                client.del('ipident:ipaddress', callback);
+            },
+
+            // loadData to redis
+            loadData: function (callback) {
+
+                client.del('ipident:ipaddress');
+
+                csv()
+                    .from.stream(fs.createReadStream(__dirname + '/data/master_ip_address.csv'), {
+                        columns: ['ip_start_num', 'ip_end_num', 'city', 'region_name',
+                                  'country_name', 'postal_code', 'latitude', 'longitude', 'metro_code',
+                                  'area_code']
+                    })
+                    .on('record', function (data) {
+                        client.zadd("ipident:ipaddress", data.ip_end_num, JSON.stringify(data));
+                    })
+                    .on('end', function () {
+                        console.log('finished loading data');
+                        if (callback) { 
+                            callback();
+                        }
+                    })
+                    .on('error', function (error) {
+                        console.log(error.message);
+                        if (callback) { 
+                            callback(error);
+                        }
+                    });
+            },
+
+            autoLoad: function (callback) {
+                client.zcount('ipident:ipaddress', '-inf', 'inf', function (err, reply) {
+                    if (reply < 1) {
+                        console.log('loading data');
+                        instance.loadData(callback);
+                    } else {
+                        console.log('data loaded');
+                        if (callback) {
+                            callback(err);
+                        }
+                    }
+                });
+            },
+
+            countData: function (callback) {
+                client.zcount('ipident:ipaddress', '-inf', 'inf', function (err, reply) {
+                    if (callback) {
+                        callback(reply);
+                    }
+                });
+            },
+
+            setRedisConfig: function (args) {
+            },
+
+            retrieveCityInfo: function (ip_address, callback) {
+                var long_ip = inet.aton(ip_address);
+                client.zrangebyscore("ipident:ipaddress", long_ip, "inf", 'limit', 0, 1, function (err, reply) {
+                    if (callback && reply.length === 1) {
+                        var data = JSON.parse(reply[0]);
+
+                        // make sure the ip_address is within range
+                        if (parseInt(data.ip_start_num, 10) <= long_ip) {
+                            callback(data);
+                        } else {
+                            data = null;
+                            callback(data);
+                        }
+                    }
+                });
+            },
+        }
+    }
+
+    return {
+        // get instance if exists, create one if doesn't
+
+        getInstance: function () {
+
+            if (!instance) {
+                instance = init();
+            }
+
+            return instance;
+        }
+    };
+
+}());
+
+module.exports = ipidentSingleton.getInstance();
 
--- ipident/test/ipident.js
+++ ipident/test/ipident.js
@@ -1,26 +1,25 @@
 /*global describe, it, before, beforeEach, after*/
 "use strict";
 
-var ipident = require('../lib/ipident'),
-    should = require('should'),
-    ipidentInst = ipident.ipidentSingleton.getInstance();
+var ipident = require('../index'),
+    should = require('should');
 
 describe('ipident start with empty data', function () {
 
     beforeEach(function (done) {
         this.timeout(0);
-        ipidentInst.clearData(done);
+        ipident.clearData(done);
     });
 
     after(function (done) {
         this.timeout(0);
-        ipidentInst.clearData(done);
+        ipident.clearData(done);
     });
 
     describe('#clearData', function () {
         it('Clear Data should empty the database', function (done) {
-            ipidentInst.clearData(function () {
-                ipidentInst.countData(function (data) {
+            ipident.clearData(function () {
+                ipident.countData(function (data) {
                     data.should.equal(0);
                     done();
                 });
@@ -32,8 +31,8 @@ describe('ipident start with empty data', function () {
         it('Load Data should fill the database', function (done) {
             this.timeout(0);
 
-            ipidentInst.loadData(function () {
-                ipidentInst.countData(function (data) {
+            ipident.loadData(function () {
+                ipident.countData(function (data) {
                     // $ wc data/master_ip_address.csv should provide the correct number
                     data.should.equal(2254131);
                     done();
@@ -45,8 +44,8 @@ describe('ipident start with empty data', function () {
     describe('#autoLoad', function () {
         it('Auto Load with empty data should call loadData', function (done) {
             this.timeout(0);
-            ipidentInst.autoLoad(function () {
-                ipidentInst.countData(function (data) {
+            ipident.autoLoad(function () {
+                ipident.countData(function (data) {
                     data.should.equal(2254131);
                     done();
                 });
@@ -59,24 +58,24 @@ describe('ipident start with filled data', function () {
 
     before(function (done) {
         this.timeout(0);
-        ipidentInst.loadData(done);
+        ipident.loadData(done);
     });
 
     after(function (done) {
         setTimeout(done, 10000);
-        ipidentInst.clearData(done);
+        ipident.clearData(done);
     });
 
     describe('autoLoad', function () {
         it('Auto Load with data already in redis should do nothing', function (done) {
             setTimeout(done, 5000);
-            ipidentInst.autoLoad(done);
+            ipident.autoLoad(done);
         });
     });
 
     describe('#countData', function () {
         it('Count Data with filled data should provide the correct count', function (done) {
-            ipidentInst.countData(function (data) {
+            ipident.countData(function (data) {
                 data.should.equal(2254131);
                 done();
             });
@@ -85,14 +84,14 @@ describe('ipident start with filled data', function () {
 
     describe('#retrieveCityInfo', function () {
         it('IP Address 125.163.49.39 should point to Bandung', function (done) {
-            ipidentInst.retrieveCityInfo('125.163.49.39', function (data) {
+            ipident.retrieveCityInfo('125.163.49.39', function (data) {
                 data.city.should.equal('Bandung');
                 done();
             });
         });
 
         it('IP Address 127.0.0.1 should not exist', function (done) {
-            ipidentInst.retrieveCityInfo('127.0.0.1', function (data) {
+            ipident.retrieveCityInfo('127.0.0.1', function (data) {
                 should.not.exist(data);
                 done();
             });
