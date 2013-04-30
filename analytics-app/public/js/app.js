f78de5a1cf4838690ce5092daede4d6f1a411462
T. Budiman <tbudiman@gmail.com>
2013-04-30 23:28:03 +0700

    change myApp to analyticsApp

--- analytics-app/public/js/app.js
+++ analytics-app/public/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-var app = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+var app = angular.module('analyticsApp', ['analyticsApp.filters', 'analyticsApp.services', 'analyticsApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2', controller: MyCtrl2});
